92bd6ad4bd19b246e6ef65437ec4dbedbd3b816e
Chayma Bahroun <72930710+cheybah@users.noreply.github.com>
2023-01-20 00:41:33 +0000

    added header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import Header from "@/app/sections/Header";
 import { Box } from "@chakra-ui/react";
 
 const App = () => {
-  return <Box>It works!</Box>;
-};
+  return <Box bg="purple.200" minH="100vh"><Header/></Box>; 
+}; 
 export default App;
